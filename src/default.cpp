c0e6dc05d00015dc67af7cc78945526091c1c0ce
kai <kai@3ab9eace-26bc-4652-8a51-1b81de405389>
2011-09-28 21:13:56 +0000

    default implementation
    git-svn-id: svn+dcn://darkcellarnights.de/kai/utils/objloader@32 3ab9eace-26bc-4652-8a51-1b81de405389

---
+++ src/default.cpp
@@ -0,0 +1,130 @@
+/* $Id$ */
+#include "default.h"
+
+#include <iostream>
+
+using namespace std;
+
+namespace obj_default 
+{
+	ObjFileLoader::ObjFileLoader(const std::string &filename, const std::string &trafo)
+	: curr_face_node(0), curr_mtl(0)
+	{
+		if (trafo != "")
+			this->trafo = lib3dmath::strtomat4f(trafo);
+		groups.push_back(Group());
+		Load(filename);
+	}
+
+	ObjFileLoader::ObjFileLoader(const std::string &filename, const lib3dmath::mat4f &trafo)
+	: curr_face_node(0), curr_mtl(0)
+	{
+		this->trafo = trafo;
+		groups.push_back(Group());
+		Load(filename);
+	}	
+
+	ObjFileLoader::ObjFileLoader(FakeMode fake, const std::string &trafo)
+	: curr_face_node(0), curr_mtl(0)
+	{
+		if (trafo != "")
+			this->trafo = lib3dmath::strtomat4f(trafo);
+		groups.push_back(Group());
+	}
+
+	ObjFileLoader::~ObjFileLoader()
+	{
+		
+	}
+
+	void ObjFileLoader::AddVertex(float x, float y, float z)
+	{
+		load_verts.push_back(lib3dmath::vec3f(x,y,z));
+	}
+
+	void ObjFileLoader::AddTexCoord(float u, float v, float w)
+	{
+		load_texs.push_back(lib3dmath::vec3f(u,v,w));
+	}
+
+	void ObjFileLoader::AddNormal(float x, float y, float z)
+	{
+		load_norms.push_back(lib3dmath::vec3f(x,y,z));
+	}
+		
+	void ObjFileLoader::AddFaceNode(int vertex, int texcoord, int normal)
+	{
+		if (curr_face_node == 3)
+		{
+			cerr << "Only triangle meshes supported!" << endl;
+			return;
+		}
+
+		if (texcoord < -1) cout << obj_filename << ": face with texcoord = " << texcoord << endl;
+
+		face_nodes[curr_face_node].x = vertex;
+		face_nodes[curr_face_node].y = texcoord;
+		face_nodes[curr_face_node].z = normal;
+		curr_face_node++;
+	}
+
+	void ObjFileLoader::AddFaceNode(enum VTN, int vertex, int texcoord, int normal)
+	{
+		AddFaceNode(vertex, texcoord, normal);
+	}
+
+	void ObjFileLoader::AddFaceNode(enum VT, int vertex, int texcoord)
+	{
+		AddFaceNode(vertex, texcoord, -1);
+	}
+
+	void ObjFileLoader::AddFaceNode(enum VN, int vertex, int normal)
+	{
+		AddFaceNode(vertex, -1, normal);
+	}
+
+	void ObjFileLoader::AddFaceNode(enum V, int vertex)
+	{
+		AddFaceNode(vertex, -1, -1);
+	}
+
+	void ObjFileLoader::FaceDone()
+	{
+		groups.back().load_idxs_v.push_back(lib3dmath::vec3i(face_nodes[0].x-1, face_nodes[1].x-1, face_nodes[2].x-1));
+		groups.back().load_idxs_t.push_back(lib3dmath::vec3i(face_nodes[0].y-1, face_nodes[1].y-1, face_nodes[2].y-1));
+		groups.back().load_idxs_n.push_back(lib3dmath::vec3i(face_nodes[0].z-1, face_nodes[1].z-1, face_nodes[2].z-1));
+		curr_face_node = 0;
+
+	}
+		
+	bool ObjFileLoader::CurrentMaterial(const std::string &name)
+	{
+		map<string, Mtl*>::iterator it = materials.find(name);
+		if (it == materials.end())
+			return false;
+
+
+		if (groups.back().load_idxs_v.size() || groups.back().load_idxs_t.size() || groups.back().load_idxs_n.size())
+		{
+			cerr << "cannot set material of a group after the first face is read." << endl;
+			return false;
+		}
+
+		curr_mtl = it->second;
+		groups.back().mat = curr_mtl;
+
+		return true;
+	}
+
+	void ObjFileLoader::StartGroup(const std::string &name)
+	{
+		if (groups.back().load_idxs_v.size() != 0)
+			groups.push_back(Group());
+		groups.back().name = name;
+		groups.back().mat = curr_mtl;
+	}
+
+}
+
+/* vim: set foldmethod=marker: */
+
---
+++ src/default.h
@@ -0,0 +1,64 @@
+#ifndef __DEFAULT_H__ 
+#define __DEFAULT_H__ 
+
+#include <string>
+#include <vector>
+#include <map>
+#include <list>
+
+#include <lib3dmath/matrix.h>
+#include "objloader.h"
+
+namespace obj_default 
+{
+	class ObjFileLoader : public ObjLoader
+	{
+	public:
+		struct Group
+		{
+			std::string name;
+			std::vector<lib3dmath::vec3i> load_idxs_v;
+			std::vector<lib3dmath::vec3i> load_idxs_n;
+			std::vector<lib3dmath::vec3i> load_idxs_t;
+			Mtl *mat;
+		};
+		enum FakeMode { FAKE };
+
+		//! beim einladen dynamisch
+		std::vector<lib3dmath::vec3f> 	load_verts;
+		std::vector<lib3dmath::vec3f> 	load_norms;
+		std::vector<lib3dmath::vec3f> 	load_texs;
+		std::list<Group>	groups;
+		int curr_face_node;
+		lib3dmath::vec3i face_nodes[3];
+
+		Mtl *curr_mtl;
+
+		bool create_mats;
+		bool create_des;
+		std::string mat_name;
+		lib3dmath::mat4f trafo;
+
+		ObjFileLoader(const std::string &filename, const std::string &trafo);
+		ObjFileLoader(const std::string &filename, const lib3dmath::mat4f &trafo);
+		ObjFileLoader(FakeMode fake, const std::string &trafo);
+		~ObjFileLoader();
+		
+		void AddVertex(float x, float y, float z);
+		void AddTexCoord(float u, float v, float w);
+		void AddNormal(float x, float y, float z);
+		
+		void AddFaceNode(int vertex, int texcoord, int normal);
+		void AddFaceNode(enum VTN, int vertex, int texcoord, int normal);
+		void AddFaceNode(enum VT,  int vertex, int texcoord);
+		void AddFaceNode(enum VN,  int vertex, int normal);
+		void AddFaceNode(enum V,   int vertex);
+		void FaceDone();
+		
+		bool CurrentMaterial(const std::string &name);
+		void StartGroup(const std::string &name);
+	};
+
+}
+
+#endif
